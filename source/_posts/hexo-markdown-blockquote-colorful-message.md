f221cb3cadf82216ed365f01bc56af2dbf9e3265
Ganlv <ganlvtech@qq.com>
2018-10-20 07:04:18 +0800

    Add markdown colorful message support.

---
+++ source/_posts/hexo-markdown-blockquote-colorful-message.md
@@ -0,0 +1,80 @@
+---
+title: Markdown 引用块加颜色作为提示框
+date: 2018-10-19 22:06:25
+tags:
+  - markdown
+  - hexo
+  - plugin
+  - bulma
+  - css
+  - sass
+  - js
+---
+
+本插件配色方案参考了 [bulma]。
+
+使用语法
+
+```markdown
+> [Warning]
+>
+> Test
+```
+
+> [Default]
+>
+> Default
+
+> [Primary]
+>
+> Primary
+
+> [Info]
+>
+> Info
+
+> [Success]
+>
+> Success
+
+> [Warning]
+>
+> Warning
+
+> [Danger]
+>
+> Danger
+
+> [Notice]
+>
+> Notice
+
+> [Tips]
+>
+> Tips
+
+> [Error]
+>
+> Error
+
+> [Primary]
+>
+> Primary
+> 
+> > [Info]
+> >
+> > Info
+> > 
+> > > [Success]
+> > >
+> > > Success
+> > > 
+> > > > [Warning]
+> > > >
+> > > > Warning
+> > > > 
+> > > > > [Danger]
+> > > > >
+> > > > > Danger
+
+[bulma]: https://bulma.io/
---
+++ themes/landscape/scripts/blockquote.js
@@ -0,0 +1,58 @@
+const cheerio = require('cheerio');
+const _ = require('lodash');
+const MESSAGE_LEVELS = {
+  // tag: 'class'
+  default: 'default',
+  primary: 'primary',
+  info: 'info',
+  success: 'success',
+  warning: 'warning',
+  danger: 'danger',
+  // alias
+  notice: 'default',
+  tips: 'info',
+  error: 'danger',
+}
+
+/**
+ * @link https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
+ */
+RegExp.escape= function(s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+hexo.extend.filter.register('after_post_render', function(data) {
+  const $ = cheerio.load(data.content);
+
+  $('blockquote').each(function(i) {
+    let $blockquote = $(this);
+    let blockquoteTextLower = _.toLower(_.trimStart($blockquote.text()));
+
+    for (let messageLevel in MESSAGE_LEVELS) {
+      let messageTag = `[${messageLevel}]`;
+
+      if (_.startsWith(blockquoteTextLower, messageTag)) {
+        let $p = $blockquote.find('p').first();
+        let textNode = $p.get(0).childNodes[0];
+
+        let messageClass = MESSAGE_LEVELS[messageLevel];
+        $blockquote.addClass('message').addClass(`is-${messageClass}`);
+        this.tagName = 'section';
+
+        if (textNode.type === 'text') {
+          let regexp = new RegExp(RegExp.escape(messageTag), 'i');
+          textNode.data = textNode.data.replace(regexp, '');
+        }
+        if ($p.text() === '') {
+          $p.remove();
+        }
+
+        break;
+      }
+    }
+  });
+
+  data.content = $.html();
+
+  return data;
+});
