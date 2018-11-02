820dfcb9c8a9f4331accfe5e0844ef447edef6ae
Ganlv <ganlvtech@qq.com>
2018-11-02 23:37:47 +0800

    fix bilibili player use https

--- scripts/bilibili.js
+++ scripts/bilibili.js
@@ -1,7 +1,7 @@
 hexo.extend.tag.register('bilibili', function(args) {
   let aid = args[0];
   let part = args[1] || 1;
-  let iframe_src = 'http://player.bilibili.com/player.html?aid=' + aid + '&part=' + part;
+  let iframe_src = 'https://player.bilibili.com/player.html?aid=' + aid + '&part=' + part;
   let a_href = 'https://www.bilibili.com/video/av' + aid;
   if (args[1]) {
     a_href += '?p=' + part;
